f264798aa0cf2226ce18317737aa2b89a1ac1ced
Ravinder Singh <ravindermaan0123@gmail.com>
2021-09-06 12:04:47 +0530

    added menu summary component

---
+++ src/components/menu/MenuSummary.js
@@ -0,0 +1,23 @@
+import Card from '../ui/Card'
+import styles from "./MenuSummary.module.css";
+
+const MenuSummary = () => {
+	return (
+		<Card className={styles.summary} dark center>
+			<section>
+				<h2>Delicious Food, Best of the Planet </h2>
+				<p>
+					Choose your favorite meal from our broad selection of
+					available meals and enjoy a delicious lunch or dinner at
+					home.
+				</p>
+				<p>
+					All our meals are cooked with high-quality ingredients,
+					just-in-time and of course by experienced chefs!
+				</p>
+			</section>
+		</Card>
+	);
+};
+
+export default MenuSummary;
