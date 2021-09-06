72735af2746312d326d795afe80bed6079e4a279
Ravinder Singh <ravindermaan0123@gmail.com>
2021-09-06 12:09:13 +0530

    integrated menu-summary component into menu component

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,23 +1,27 @@
 import Card from "../ui/Card";
 import MenuItem from "./MenuItem";
+import MenuSummary from "./MenuSummary";
 
-import styles from './Menu.module.css'
+import styles from "./Menu.module.css";
 
-const Menu = ({ items,onAdd:handleAdd }) => {
+const Menu = ({ items, onAdd: handleAdd }) => {
 	return (
-		<Card className={styles.menu_list}>
-			{items.map((val) => (
-				<MenuItem
-					key={val.id}
-					id={val.id}
-					title={val.title}
-					subtitle={val.tagline}
-					price={val.price}
-					count={val.count}
-					onAdd={handleAdd}
-				/>
-			))}
-		</Card>
+		<>
+			<MenuSummary />
+			<Card className={styles.menu_list}>
+				{items.map((val) => (
+					<MenuItem
+						key={val.id}
+						id={val.id}
+						title={val.title}
+						subtitle={val.tagline}
+						price={val.price}
+						count={val.count}
+						onAdd={handleAdd}
+					/>
+				))}
+			</Card>
+		</>
 	);
 };
 
