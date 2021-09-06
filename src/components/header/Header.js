41e6521c5e53047d7f3494ac196183ae76f797bc
Ravinder Singh <ravindermaan0123@gmail.com>
2021-09-06 11:18:39 +0530

    added a img in header component that will be displayed under header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,21 @@
-import CartButton from '../cart/CartButton'
+import CartButton from "../cart/CartButton";
 import styles from "./Header.module.css";
 
-const Header = ({cartData,onCartOpen}) => {
+import mealImg from '../../assets/meals.jpg'
+
+const Header = ({ cartData, onCartOpen }) => {
 	return (
-		<header className={styles.header}>
-			<div className={styles.contents}>
-				<span className={styles.title}>ReactMeals</span>
-				<CartButton count={cartData.length} onClick={onCartOpen}/>
+		<>
+			<header className={styles.header}>
+				<div className={styles.contents}>
+					<span className={styles.title}>ReactMeals</span>
+					<CartButton count={cartData.length} onClick={onCartOpen} />
+				</div>
+			</header>
+			<div className={styles['main-image']}>
+				<img src={mealImg} alt="Meal Image" />
 			</div>
-		</header>
+		</>
 	);
 };
 
