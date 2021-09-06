b37d08db409af464726c2908d91759a3d4899fb0
Ravinder Singh <ravindermaan0123@gmail.com>
2021-09-06 10:17:17 +0530

    added a Cart component
    this component uses cart item component to render all items present in
    cart

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,46 @@
+import CartItem from "./CartItem";
+import Modal from "../ui/Modal";
+import Card from "../ui/Card";
+import Button from "../ui/Button";
+
+import styles from "./Cart.module.css";
+
+const Cart = ({ cartData, onInc, onDec, isOpen, onClose: closeCart }) => {
+
+	let totalPrice = cartData
+		.reduce((total, val) => total + val.price * val.count, 0)
+		.toFixed(2);
+
+	const handleOrder = ()=>{
+		console.log('Ordering..')
+		console.log('Total Price:',totalPrice)
+	}
+
+	return (
+		<Modal visible={isOpen}>
+			<Card>
+				{cartData.map((val) => (
+					<CartItem
+						key={val.id}
+						id={val.id}
+						title={val.title}
+						price={val.price}
+						count={val.count}
+						onAdd={onInc}
+						onRemove={onDec}
+					/>
+				))}
+				<div className={styles.total}>
+					<span>Total Amount</span>
+					<span>$ {totalPrice}</span>
+				</div>
+				<div className={styles.actions}>
+					<Button title="Cancel" onClick={closeCart} outline />
+					<Button title="Order" disabled={totalPrice===0} onClick={handleOrder}/>
+				</div>
+			</Card>
+		</Modal>
+	);
+};
+
+export default Cart;
