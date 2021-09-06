59a5899c9f8be77c9a06c92f7f9c1c2ed6a3e0a2
Ravinder Singh <ravindermaan0123@gmail.com>
2021-09-06 10:34:53 +0530

    Integrated all components in APP.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Header from "./components/header/Header";
+import Menu from "./components/menu/Menu";
+import Cart from "./components/cart/Cart";
+
+import "./App.css";
+
+const rawData = [
+    {
+        id: "1a",
+        title: "Sushi",
+        tagline: "Finest fish with veggies",
+        price: 22.99,
+    },
+    {
+        id: "2a",
+        title: "Schnitzel",
+        tagline: "A german specialty!",
+        price: 16.5,
+    },
+    {
+        id: "3a",
+        title: "Barbecue Burger",
+        tagline: "American,raw,meaty",
+        price: 12.99,
+    },
+    {
+        id: "4a",
+        title: "Green Bowl",
+        tagline: "Healthy ...and green ...",
+        price: 18.99,
+    },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [menuData, setMenuData] = useState(rawData);
+    const [cartItems, setCartItems] = useState([]);
+    const [cartOpen,setCartOpen] = useState(false);
+
+    const toggleCartOpen = ()=>{
+        setCartOpen(!cartOpen)
+    }
+
+    const handleAddToCart = (item) => {
+        if (cartItems.filter((val) => val.id === item.id).length > 0) return;
+        setCartItems((prev) => [...prev, item]);
+    };
+
+    const incrementCart = (id) => {
+        setCartItems((prev) =>
+            prev.map((val) =>
+                val.id === id
+                    ? Object.assign({}, val, { count: val.count + 1 })
+                    : val
+            )
+        );
+    };
+
+    const decrementCart = (id) => {
+        setCartItems((prev) =>
+            prev.map((val) =>
+                val.id === id
+                    ? Object.assign({}, val, { count: val.count ? val.count-1: 0 })
+                    : val
+            )
+        );
+    };
+
+    return (
+        <div className="App">
+            <Header cartData={cartItems} onCartOpen={toggleCartOpen}/>
+            <Menu items={menuData} onAdd={handleAddToCart} />
+            <Cart
+                cartData={cartItems}
+                onInc={incrementCart}
+                onDec={decrementCart}
+                isOpen={cartOpen}
+                onClose={toggleCartOpen}
+            />
+        </div>
+    );
 }
 
 export default App;
